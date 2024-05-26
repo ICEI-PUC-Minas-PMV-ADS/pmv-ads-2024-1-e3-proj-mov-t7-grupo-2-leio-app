8cffcf1f4a148c6e6eb3e4dee5913b726f52a2d0
alineazedias <aline.azedias@gmail.com>
2024-05-26 14:52:34 -0300

    modal

--- src/leio/Components/Modal.js
+++ src/leio/Components/Modal.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Image, Modal as RNModal } from 'react-native';
 import styles from "../assets/styles/base";
 import styleModal from "../assets/styles/modal";
 
@@ -76,36 +76,42 @@ const Modal = ({ navigation }) => {
     };
 
     return (
-        <View style={styleModal.container}>
-            <Text style={styleModal.headerText}>Marcar como:</Text>
+        <RNModal
+            animationType="none"
+            transparent={true}
+            visible={true}
+        >
+            <View style={styleModal.modalBackground}>
+                <View style={styleModal.container}>
+                    <Text style={styleModal.headerText}>Marcar como:</Text>
 
-            <View style={styleModal.optionContainer}>
-                <TouchableOpacity style={styleModal.option} onPress={toggleImage1}>
-                    <Image source={image1} style={styleModal.icon} />
-                    <Text style={styleModal.optionText}>Lido</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styleModal.option} onPress={toggleImage2}>
-                    <Image source={image2} style={styleModal.icon} />
-                    <Text style={styleModal.optionText}>Quero ler</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styleModal.option} onPress={toggleImage3}>
-                    <Image source={image3} style={styleModal.icon} />
-                    <Text style={styleModal.optionText}>Relendo</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styleModal.option} onPress={toggleImage4}>
-                    <Image source={image4} style={styleModal.icon} />
-                    <Text style={styleModal.optionText}>Abandonei</Text>
-                </TouchableOpacity>
+                    <View style={styleModal.optionContainer}>
+                        <TouchableOpacity style={styleModal.option} onPress={toggleImage1}>
+                            <Image source={image1} style={styleModal.icon} />
+                            <Text style={styleModal.optionText}>Lido</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styleModal.option} onPress={toggleImage2}>
+                            <Image source={image2} style={styleModal.icon} />
+                            <Text style={styleModal.optionText}>Quero ler</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styleModal.option} onPress={toggleImage3}>
+                            <Image source={image3} style={styleModal.icon} />
+                            <Text style={styleModal.optionText}>Relendo</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styleModal.option} onPress={toggleImage4}>
+                            <Image source={image4} style={styleModal.icon} />
+                            <Text style={styleModal.optionText}>Abandonei</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styleModal.ratingContainer}>
+                        {renderStars()}
+                    </View>
+
+                    <Button title="Close" onPress={() => navigation.goBack()} />
+                </View>
             </View>
-
-            <View style={styleModal.ratingContainer}>
-                {renderStars()}
-            </View>
-
-            <Button title="Close" onPress={() => navigation.goBack()} />
-        </View>
+        </RNModal>
     );
 };
-
 export default Modal;
-
